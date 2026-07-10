224f2e3ba44d04680ded9b3382ba273fcd2b4e5e
zero-jdit <zerosjd@gmail.com>
2026-07-10 18:00:32 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -18,9 +18,8 @@ const nextConfig: NextConfig = {
         pathname: '/**', // This allows any path under the hostname
       },
     ],
-    unoptimized: true,
   },
-  output: 'export',
+  output: 'standalone',
   transpilePackages: ['motion'],
   webpack: (config, {dev}) => {
     // HMR is disabled in AI Studio via DISABLE_HMR env var.
