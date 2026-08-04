dcf6b378bb0c0455036276aebb0de7ba705be425
zero-jdit <zerosjd@gmail.com>
2026-08-04 11:06:50 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  output: 'standalone',
+  output: 'export',
   transpilePackages: ['motion'],
   webpack: (config, {dev}) => {
     // HMR is disabled in AI Studio via DISABLE_HMR env var.
